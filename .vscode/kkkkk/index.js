adeff89f08df0b07fc04ab543f40563de2009912
Mr Usty <ustyventures@gmail.com>
2024-10-21 17:16:07 +0100

    my files

---
+++ .vscode/kkkkk/index.js
@@ -0,0 +1,10 @@
+const buttonEl = document.querySelector("button");
+const inputEl = document.querySelector("date");
+buttonEl.addEventListener("click", () =>{
+updateButton()
+})
+
+function updateButton() {
+
+    
+}
